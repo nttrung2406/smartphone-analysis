86fab559df52a8bdcd1d494dffd2be871a742808
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2024-10-17 21:22:14 +0700

    Create Footer.js

---
+++ src/component/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{
+      background: 'linear-gradient(90deg, #00008B, #000000)',
+      padding: '10px',
+      textAlign: 'center',
+      color: 'white',
+      position: 'fixed',
+      bottom: 0,
+      width: '100%',
+      fontFamily: 'Orbitron, sans-serif',
+    }}>
+      <p>&copy; By Trung Nguyen</p>
+    </footer>
+  );
+};
+
+export default Footer;
